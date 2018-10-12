1842e5fd4ee1103073fef0b0280d5003368abdae
NoahGiles <billywerber@gmail.com>
2018-10-12 09:52:09 -0500

    Updated Text

--- FinalProject/src/main/casinoMain.java
+++ FinalProject/src/main/casinoMain.java
@@ -10,18 +10,19 @@ public class casinoMain {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		int gameChoice = 0;
-		System.out.println("                                          Welcome to the                                                          ");
-		System.out.println("                                                                                                                  ");
-		System.out.println("   0000000000000000   0000000000000000   0000000000000000   0000000000000000   00000000   00000   000000000000000  ");
-		System.out.println("   0000000000000000   0000000000000000   0000000000000000   0000000000000000   00000000   00000   000000000000000 ");
-		System.out.println("   000                000          000   000000000000            000000        0000000000 00000   000         000 ");
-		System.out.println("   000                000          000    00000000000000         000000        0000000000000000   000    0    000");
+		System.out.println("                                          Welcome to the                                                         ");
+		System.out.println("                                                                                                                 ");
+		System.out.println("                                                                                                                 ");
+		System.out.println("   0000000000000000   0000000000000000   0000000000000000   0000000000000000   00000000   00000   000000000000000");
+		System.out.println("   0000000000000000   0000000000000000   0000000000000000   0000000000000000   00000000   00000   000000000000000");
+		System.out.println("   000                000          000   000000000000            000000        0000000000 00000   000         000");
+		System.out.println("   000                000          000    00000000000000         000000        0000000000000000   000         000");
 		System.out.println("   000                0000000000000000        00000000000        000000        00000  000000000   000         000");
 		System.out.println("   0000000000000000   0000000000000000   0000000000000000   0000000000000000   00000   00000000   000000000000000");
 		System.out.println("   0000000000000000   000          000   0000000000000000   0000000000000000   00000    0000000   000000000000000");
 		
-		System.out.println("\n\nPlease choose a game : (1) Blackjack (2) Craps (3) Slot Machine");
-		gameChoice = input.nextInt();
+		System.out.println("\n\nPlease choose a game : (1) Blackjack (2) Roulette (3) Slot Machine");
+//		gameChoice = input.nextInt();
 		
 		Slots.randomGen();
 	}
