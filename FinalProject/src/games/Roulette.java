a6121f547c16b6bfb9cecd9bb17dafb8a6a8179d
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-10-19 09:23:21 -0500

    update roullette

--- FinalProject/src/games/Roulette.java
+++ FinalProject/src/games/Roulette.java
@@ -1,12 +1,75 @@
 package games;
 
-public class roulette {
+import java.util.Scanner;
 
-	public static void main(String[] args) 
+import java.util.Random;
+
+public class roulette {
+	
+	public static void roulette() {
 	// TODO Auto-generated method stub
-	{
+		System.out.println("Welcome to the Roulette Game");
+		int userGuess;
+		int computerSecret;
+		int userChoice;
+		int userPick;
+		String red;
+		String black;
+		String green;
+		int money = 0;
+		int amount;
+		int again = 0;
+		String result = null;
 		
+		Scanner input = new Scanner(System.in);
+		
+		do
+		{
+
+			computerSecret = (int)(Math.random() * 36);
+			System.out.println("How much money are you betting:");
+			money = input.nextInt();
+		System.out.println("Pick any number from 0 to 36:");
+		userGuess = input.nextInt();
+		System.out.println("1-black\2-red");
+		userChoice = input.nextInt();
+		if(computerSecret % 2 == 0) 
+		{
+			result = "Black " + computerSecret;
+			if(userChoice == 1) {
+				System.out.println(" you win ! " + userGuess);
+			}
+			else {
+				System.out.println("You lose ! " + userGuess);
+			}
+		}
+		else if(computerSecret % 2 >= 1) {
+			result = "Red " + computerSecret;
+			if(userChoice == 2){
+				System.out.println(" you win ! " + userGuess) ;
+				
+			}
+			else {
+				System.out.println(" you lose ! "+ userGuess);
+			}
+		}
+		
+		displayMessage(result);
+		
+		System.out.println("Do you want to play again ?\n1- for yes\n2- for no >>");
+		again = input.nextInt();
+		}while(again == 1);
+	}
+public static void displayMessage(String result)
+{
+{
+	System.out.println(result);
+}
+}
 
 	}
 
-}
+		
+
+	    
+		 
